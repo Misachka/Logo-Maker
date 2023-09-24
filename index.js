51a0f5087395f49c0d0432a43c9e9ed19efe4d5b
Misachka <136197530+Misachka@users.noreply.github.com>
2023-09-24 15:46:43 -0400

    created function promt and defiend questions

--- index.js
+++ index.js
@@ -42,5 +42,49 @@ function writeToFile(fileName, answers) {
 }
 
 
+//function uses inqueirer, to ask questions to user so the logo will be generated
+function promptUser() {
+    inquirer
+      .prompt([
+       
+        {
+          type: "input",
+          message:
+            "What text would you like you logo to display? (Enter up to three characters)",
+          name: "text",
+        },
+
+        {
+          type: "input",
+          message:
+            "Choose text color (Enter color keyword OR a hexadecimal number)",
+          name: "textColor",
+        },
+
+        {
+          type: "list",
+          message: "What shape would you like the logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+
+        {
+          type: "input",
+          message:
+            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+          name: "shapeBackgroundColor",
+        },
+      ])
+      .then((answers) => {
+        // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+        if (answers.text.length > 3) {
+          console.log("Must enter a value of no more than 3 characters");
+          promptUser();
+        } else {
+          // Calling write file function to generate SVG file
+          writeToFile("logo.svg", answers);
+        }
+      });
+  }
 
 promptUser();
